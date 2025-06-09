915c9e6f2da57e21e80c4a801e9b902bf38d0d9e
jameszhao93 <jameszhao1021@gmail.com>
2025-06-10 03:13:46 +0800

    ensure CreatedAt is removed

---
+++ LuxeStays.Infrastructure/Migrations/20250609191259_removeFinal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LuxeStays.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeFinal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
